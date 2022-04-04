e78930b7184b7a57b624fa26c4d9969a9fa6e6c1
Aleksey Sivaks <koshmariel@gmail.com>
2022-04-05 01:19:47 +0400

    refactored id & class

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTests {
     @BeforeAll
-    static void setUP() {
+    static void setUp() {
 //        System.out.println("### Before All");
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
@@ -37,49 +37,47 @@ public class PracticeFormTests {
 
         open("/automation-practice-form");
 
-        $("[id=firstName]").setValue(firstName);
-        $("[id=lastName]").setValue(lastName);
-        $("[id=userEmail]").setValue(email);
-        $("[id=genterWrapper]").$(byText(gender)).click();
-        $("[id=userNumber]").setValue(phone);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phone);
 
         //Does not work
         //$("[id=dateOfBirthInput]").clear();
 
 
         //DATE OF BIRTH BLOCK START
-        $("[id=dateOfBirthInput]").sendKeys((Keys.COMMAND + "a"));
-        $("[id=dateOfBirthInput]").sendKeys(Keys.SPACE);
-        $("[id=dateOfBirthInput]").setValue(dateOfBirthType).pressEnter();
+        $("#dateOfBirthInput").sendKeys((Keys.COMMAND + "a"));
+        $("#dateOfBirthInput").sendKeys(Keys.SPACE);
+        $("#dateOfBirthInput").setValue(dateOfBirthType).pressEnter();
         //DATE OF BIRTH BLOCK END
 
         //<div class="subjects-auto-complete__control css-yk16xz-control"><div class="subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3"><div class="subjects-auto-complete__placeholder css-1wa3eu0-placeholder"></div><div class="css-1g6gooi"><div class="subjects-auto-complete__input" style="display: inline-block;"><input autocapitalize="none" autocomplete="off" autocorrect="off" id="subjectsInput" spellcheck="false" tabindex="0" type="text" aria-autocomplete="list" value="" style="box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 1; outline: 0px; padding: 0px; color: inherit;"><div style="position: absolute; top: 0px; left: 0px; visibility: hidden; height: 0px; overflow: scroll; white-space: pre; font-size: 16px; font-family: -apple-system, &quot;system-ui&quot;, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;; font-weight: 400; font-style: normal; letter-spacing: normal; text-transform: none;"></div></div></div></div><div class="subjects-auto-complete__indicators css-1wy0on6"><span class="subjects-auto-complete__indicator-separator css-1okebmr-indicatorSeparator"></span></div></div>
-        $("[id=subjectsInput]").setValue(subjects).pressEnter();
+        $("#subjectsInput").setValue(subjects).pressEnter();
 
         //If browser window would be to short scroll will fail
         //$("[id='submit']").scrollIntoView(true);
 
-        $("[id=hobbiesWrapper]").$(byText(hobbies)).click();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
         $("#uploadPicture").uploadFromClasspath(imgPath);
-        $("[id=currentAddress]").setValue(currAddr);
+        $("#currentAddress").setValue(currAddr);
 
-        //$("[id=currentAddress]").sendKeys((Keys.COMMAND + "-"));
-        //$("[id=currentAddress]").sendKeys((Keys.COMMAND + "-"));
-        //$("[id=currentAddress]").sendKeys((Keys.COMMAND + "-"));
+        //DOES NOT WORK
         //$("[id=currentAddress]").sendKeys((Keys.COMMAND + "-"));
         //$("[id=currentAddress]").sendKeys((Keys.COMMAND + Keys.SUBTRACT));
 
 
-        $("[id=state]").click();
-        $("[id=stateCity-wrapper]").$(byText(state)).click();
-        $("[id=city]").click();
-        $("[id=stateCity-wrapper]").$(byText(city)).click();
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
 
-        $("[id=submit]").click();
+        $("#submit").click();
 
         //ASSERTS
-        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
-        $("[class=table-responsive]").shouldHave(
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
                 text(firstName + ' ' + lastName),
                 text(email),
                 text(gender),
@@ -116,25 +114,25 @@ public class PracticeFormTests {
 
         open("/automation-practice-form");
 
-        $("[id=firstName]").setValue(firstName);
-        $("[id=lastName]").setValue(lastName);
-        $("[id=userEmail]").setValue(email);
-        $("[id=genterWrapper]").$(byText(gender)).click();
-        $("[id=userNumber]").setValue(phone);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phone);
 
         //Does not work
         //$("[id=dateOfBirthInput]").clear();
 
         //DATE OF BIRTH BLOCK START
-        $("[id=dateOfBirthInput]").click();
-        $("[class=react-datepicker__month-select]").selectOption(dateOfBirthMonthFull);
-        $("[class=react-datepicker__year-select]").selectOption(dateOfBirthMonthYear);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(dateOfBirthMonthFull);
+        $(".react-datepicker__year-select").selectOption(dateOfBirthMonthYear);
 
         //will also work
         //$("[aria-label$='Choose Saturday, December 2nd, 2000']").click();
 
         //"02"->"2"
-        $("[class=react-datepicker__month]").$(byText(Integer.toString(
+        $(".react-datepicker__month").$(byText(Integer.toString(
                                 Integer.parseInt(dateOfBirthMonthDay)
                         )
                 )
@@ -142,25 +140,25 @@ public class PracticeFormTests {
         //DATE OF BIRTH BLOCK END
 
         //<div class="subjects-auto-complete__control css-yk16xz-control"><div class="subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3"><div class="subjects-auto-complete__placeholder css-1wa3eu0-placeholder"></div><div class="css-1g6gooi"><div class="subjects-auto-complete__input" style="display: inline-block;"><input autocapitalize="none" autocomplete="off" autocorrect="off" id="subjectsInput" spellcheck="false" tabindex="0" type="text" aria-autocomplete="list" value="" style="box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 1; outline: 0px; padding: 0px; color: inherit;"><div style="position: absolute; top: 0px; left: 0px; visibility: hidden; height: 0px; overflow: scroll; white-space: pre; font-size: 16px; font-family: -apple-system, &quot;system-ui&quot;, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;; font-weight: 400; font-style: normal; letter-spacing: normal; text-transform: none;"></div></div></div></div><div class="subjects-auto-complete__indicators css-1wy0on6"><span class="subjects-auto-complete__indicator-separator css-1okebmr-indicatorSeparator"></span></div></div>
-        $("[id=subjectsInput]").setValue(subjects).pressEnter();
+        $("#subjectsInput").setValue(subjects).pressEnter();
 
         //If browser window would be to short scroll will fail
-        //$("[id='submit']").scrollIntoView(true);
+        //$("[id='submit'").scrollIntoView(true);
 
-        $("[id=hobbiesWrapper]").$(byText(hobbies)).click();
-        $("[id=uploadPicture]").uploadFromClasspath(imgPath);
-        $("[id=currentAddress]").setValue(currAddr);
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath(imgPath);
+        $("#currentAddress").setValue(currAddr);
 
-        $("[id=state]").click();
-        $("[id=stateCity-wrapper]").$(byText(state)).click();
-        $("[id=city]").click();
-        $("[id=stateCity-wrapper]").$(byText(city)).click();
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
 
-        $("[id=submit]").click();
+        $("#submit").click();
 
         //ASSERTS
-        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
-        $("[class=table-responsive]").shouldHave(
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
                 text(firstName + ' ' + lastName),
                 text(email),
                 text(gender),
